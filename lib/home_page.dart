07a72d0ab4db4e432752f83df49483db5fba7c68
Olim <75718252+java0818@users.noreply.github.com>
2022-12-08 10:22:19 +0500

    0012

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: prefer_if_null_operators
 
 import 'package:flutter/material.dart';
+import 'package:servir1/post/post_model.dart';
 import 'package:servir1/services/http_services.dart';
-
-//import 'package:http/http.dart';
+import 'package:http/http.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,28 +14,40 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  late String data;
+  late String? data;
+  //String? data;
 
   void _apiPostList(){
-    Network.GET(Network.API_LIST, Network.paramsEmpty()).then((respons) => {
-      // ignore: avoid_print
-      print(respons),
-      _showResponse(respons),
+    Network.GET(Network.API_LIST, Network.paramsEmpty()).then((response) => {
+      print(response),
+      _showResponse(response),
+    });
+  }
+
+  void _apiCreatePost(Post post){
+    Network.POST(Network.API_CREAT, Network.paramsCreate(post)).then((response) => {
+      print(response),
+      _showResponse(response),
     });
   }
 
 
-  void _showResponse(String respons){
+  void _showResponse(String? response){
     setState(() {
-      data = respons;
+      if(response!=null){
+        data = response;
+      }else{
+        data="no data";
+      }
     });
   }
 
   @override
   void initState() {
-    // TODO: implement initState
+    // TODO: implement; initState
     super.initState();
-    _apiPostList();
+    var post; Post(id: 1,title: "title",body: "body",userId: 1);
+    _apiCreatePost(post );
   }
 
   @override
---
+++ lib/post/post_model.dart
@@ -0,0 +1,26 @@
+
+
+import 'dart:convert';
+
+class Post{
+  late int id;
+  late String title;
+  late String body;
+  late int userId;
+
+  Post({required this.id, required this.body,required this.title,required this.userId});
+
+  Post.fromJson(Map<String,dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        body = json['body'],
+        userId = json['userId'];
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'body': body,
+    'userId': userId,
+  };
+
+}
